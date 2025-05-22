25e9ae4221560a015ef90fc2e0e4deaac2d48cb5
이지은 <tori@ryanlab.kr>
2025-05-22 20:37:56 +0900

    초기세팅 & 헤더 GNB 메뉴 Splitting 을 이용한 마우스호버 효과 & 헤더 스크롤 방향 이벤트 적용

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// 01. 스플리팅 호출
+$(function () {
+    Splitting();
+});
+
+// 02. 헤더 영역 스크롤 방향 이벤트
+$(function () {
+    var prevScrollTop = 0;
+
+    document.addEventListener("scroll", function () {
+        var nowScrollTop = $(window).scrollTop();
+
+        if (nowScrollTop > prevScrollTop) {
+            $("header").addClass("active");
+        } else {
+            $("header").removeClass("active");
+        }
+
+        prevScrollTop = nowScrollTop;
+    });
+});
