b7c24bf57b87d400848e23094f7fbfc0dc59a2b0
이지은 <tori@ryanlab.kr>
2025-05-23 07:16:02 +0900

    ClipPath 로 Gsap Animation 제작

--- js/script.js
+++ js/script.js
@@ -27,3 +27,37 @@ $(function () {
         once: false,
     });
 });
+
+// 04. con01 gsap 애니메이션
+$(function () {
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: ".con01",
+            start: "0% 80%", // 트리거 대상, 브라우저
+            end: "100% 100%", // 트리거 대상, 브라우저
+            scrub: 1,
+            markers: true,
+        },
+    })
+        .to(
+            ".wrap",
+            {
+                backgroundColor: "#fff",
+                color: "#000",
+                ease: "none",
+                duration: 5,
+            },
+            0
+        )
+        .to(".scroll", { opacity: "0", ease: "none", duration: 0 })
+        .fromTo(
+            ".videoWrap video",
+            { "clip-path": "inset(60% 60% 60% 60% round 30%)" },
+            {
+                "clip-path": "inset(0% 0% 0% 0% round 0%)",
+                ease: "none",
+                duration: 10,
+            },
+            0
+        );
+});
