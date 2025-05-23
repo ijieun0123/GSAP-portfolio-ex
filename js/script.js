e35d6b151d87f8ee7e6ca6d09daa24e746de5611
이지은 <tori@ryanlab.kr>
2025-05-23 10:33:48 +0900

    con02 영역 마우스 오버, scrollTrigger 제작

--- js/script.js
+++ js/script.js
@@ -60,4 +60,89 @@ $(function () {
             },
             0
         );
+
+    // 05. con02 gsap 애니메이션
+    // my work title
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: ".con02",
+            start: "0% 100%",
+            end: "0% 20%",
+            scrub: 1,
+            // markers: true,
+        },
+    })
+        .fromTo(
+            ".con02 .title .a",
+            { x: "-100%" },
+            { x: "0%", ease: "none", duration: 5 },
+            0
+        )
+        .fromTo(
+            ".con02 .title .b",
+            { x: "100%" },
+            { x: "0%", ease: "none", duration: 5 },
+            0
+        );
+
+    // workList 가 나타날 때 배경색 검정으로
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: ".workList",
+            start: "0% 100%",
+            end: "0% 100%",
+            scrub: 1,
+            // markers: true,
+        },
+    })
+        .to(
+            ".wrap",
+            {
+                backgroundColor: "#000",
+                color: "#fff",
+                ease: "none",
+                duration: 5,
+            },
+            0
+        )
+
+        // title - position fixed 적용
+        .to(
+            ".con02 .title",
+            {
+                position: "fixed",
+                ease: "none",
+                left: "0",
+                top: "0",
+                width: "100%",
+                duration: 5,
+            },
+            0
+        )
+
+        // workList 에 margin-top 을 적용해서 애니를 자연스럽게
+        .fromTo(
+            ".workList",
+            { margin: "0 auto" },
+            {
+                margin: "100vh auto 0",
+                position: "relative",
+                zIndex: "10",
+                duration: 1,
+            },
+            0
+        );
+
+    // workList 가 끝날 때 title 이 화면 밖으로 사라지도록
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: ".workList",
+            start: "100% 50%",
+            end: "100% 0%",
+            scrub: 1,
+            // markers: true,
+        },
+    })
+        .to(".con02 .title .a", { x: "-100%", ease: "none", duration: 5 }, 0)
+        .to(".con02 .title .b", { x: "100%", ease: "none", duration: 5 }, 0);
 });
